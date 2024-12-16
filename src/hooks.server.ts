29297951ef2317d33ebc9760cdc5e0c2f59ee761
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2024-12-16 19:02:06 +0100

    inital commit

---
+++ src/hooks.server.ts
@@ -0,0 +1,47 @@
+import type { Handle } from '@sveltejs/kit';
+import { i18n } from '@/i18n';
+import { sequence } from '@sveltejs/kit/hooks';
+import { lucia } from '@/server/auth';
+import { dev } from '$app/environment';
+import { securityHeaders, rules } from '@islamzaoui/securekit';
+import { ORIGIN } from '$env/static/private';
+
+const handleParaglide: Handle = i18n.handle();
+
+const handleAuth: Handle = async ({ event, resolve }) => {
+	const sessionId = event.cookies.get(lucia.sessionCookieName);
+	if (!sessionId) {
+		event.locals.user = null;
+		event.locals.session = null;
+		return resolve(event);
+	}
+
+	const { session, user } = await lucia.validateSession(sessionId);
+	if (session && session.fresh) {
+		const sessionCookie = lucia.createSessionCookie(session.id);
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+	}
+	if (!session) {
+		const sessionCookie = lucia.createBlankSessionCookie();
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+	}
+	event.locals.user = user;
+	event.locals.session = session;
+	return resolve(event);
+};
+
+const origin = dev ? 'http://localhost:5173' : ORIGIN;
+const handleSecurityHeaders = securityHeaders({
+	headers: {
+		...rules.defaultHeaders,
+		'Access-Control-Allow-Origin': origin
+	}
+}).handle;
+
+export const handle: Handle = sequence(handleParaglide, handleAuth, handleSecurityHeaders);
